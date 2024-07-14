942e9a6ab303ce54ecb50045d7ab31147a6d91d2
Naoufel <nberrada@lithium.paris>
2024-07-14 12:20:09 +0200

    fix: fuzzy search

--- src/lib.rs
+++ src/lib.rs
@@ -4,23 +4,18 @@ use std::collections::{HashMap, HashSet};
 
 const EMBEDDING_SIZE: usize = 1000;
 const DEFAULT_SIMILARITY_THRESHOLD: f32 = 0.1;
-const NGRAM_SIZE: usize = 3;
-const WORD_WEIGHT: f32 = 0.7;
-const NGRAM_WEIGHT: f32 = 0.3;
+const NGRAM_SIZES: [usize; 3] = [2, 3, 4];
 
 #[derive(Serialize, Deserialize, Clone)]
 struct IndexedDocument {
   path: String,
-  word_vector: Vec<f32>,
   ngram_vector: Vec<f32>,
 }
 
 #[derive(Serialize, Deserialize, Clone)]
 struct IndexData {
   document_vectors: Vec<IndexedDocument>,
-  word_vocabulary: HashSet<String>,
   ngram_vocabulary: HashSet<String>,
-  word_idf: HashMap<String, f32>,
   ngram_idf: HashMap<String, f32>,
 }
 
@@ -77,9 +72,7 @@ impl VectorizationSystem {
           .map_err(|e| JsValue::from_str(&format!("Failed to parse index data: {}", e)))?,
       None => IndexData {
         document_vectors: Vec::new(),
-        word_vocabulary: HashSet::new(),
         ngram_vocabulary: HashSet::new(),
-        word_idf: HashMap::new(),
         ngram_idf: HashMap::new(),
       },
     };
@@ -90,23 +83,21 @@ impl VectorizationSystem {
     })
   }
 
+
   /// Searches for documents similar to the given query.
   /// @param query - The search query.
   /// @returns An array of MatchResult objects representing the search results.
   #[wasm_bindgen(js_name = search)]
   pub fn search(&self, query: &str) -> Result<MatchResultArrayJS, JsValue> {
-    let (query_word_vector, query_ngram_vector) = self.text_to_vectors(query);
+    let query_vector = self.text_to_vector(query);
     let mut results = Vec::new();
 
     for doc in &self.index_data.document_vectors {
-      let word_similarity = cosine_similarity(&query_word_vector, &doc.word_vector);
-      let ngram_similarity = cosine_similarity(&query_ngram_vector, &doc.ngram_vector);
-      let combined_similarity = WORD_WEIGHT * word_similarity + NGRAM_WEIGHT * ngram_similarity;
-
-      if combined_similarity > self.similarity_threshold {
+      let similarity = cosine_similarity(&query_vector, &doc.ngram_vector);
+      if similarity > self.similarity_threshold {
         results.push(MatchResult {
           is_match: true,
-          similarity: combined_similarity,
+          similarity,
           path: doc.path.clone(),
         });
       }
@@ -121,31 +112,19 @@ impl VectorizationSystem {
   /// Converts text to a vector representation.
   /// @param text - The input text to vectorize.
   /// @returns A vector (Float32Array) representation of the input text.
-  fn text_to_vectors(&self, text: &str) -> (Vec<f32>, Vec<f32>) {
-    let words = self.tokenize(text);
+  fn text_to_vector(&self, text: &str) -> Vec<f32> {
     let ngrams = self.generate_ngrams(text);
-
-    let mut word_vector = vec![0.0; EMBEDDING_SIZE];
-    let mut ngram_vector = vec![0.0; EMBEDDING_SIZE];
-
-    for word in words {
-      if let Some(&idf) = self.index_data.word_idf.get(&word) {
-        let index = self.hash(&word) % EMBEDDING_SIZE;
-        word_vector[index] += idf;
-      }
-    }
+    let mut vector = vec![0.0; EMBEDDING_SIZE];
 
     for ngram in ngrams {
       if let Some(&idf) = self.index_data.ngram_idf.get(&ngram) {
         let index = self.hash(&ngram) % EMBEDDING_SIZE;
-        ngram_vector[index] += idf;
+        vector[index] += idf;
       }
     }
 
-    normalize_vector(&mut word_vector);
-    normalize_vector(&mut ngram_vector);
-
-    (word_vector, ngram_vector)
+    normalize_vector(&mut vector);
+    vector
   }
 
   /// Adds a document to the index.
@@ -153,18 +132,10 @@ impl VectorizationSystem {
   /// @param text - The content of the document.
   #[wasm_bindgen(js_name = addDocument)]
   pub fn add_document(&mut self, path: &str, text: &str) {
-    let words = self.tokenize(text);
     let ngrams = self.generate_ngrams(text);
-
-    let unique_words: HashSet<_> = words.into_iter().collect();
     let unique_ngrams: HashSet<_> = ngrams.into_iter().collect();
 
-    // Update vocabularies and document frequencies
-    for word in &unique_words {
-      self.index_data.word_vocabulary.insert(word.clone());
-      *self.index_data.word_idf.entry(word.clone()).or_insert(0.0) += 1.0;
-    }
-
+    // Update vocabulary and document frequency
     for ngram in &unique_ngrams {
       self.index_data.ngram_vocabulary.insert(ngram.clone());
       *self.index_data.ngram_idf.entry(ngram.clone()).or_insert(0.0) += 1.0;
@@ -172,18 +143,14 @@ impl VectorizationSystem {
 
     // Update IDF values
     let total_docs = (self.index_data.document_vectors.len() + 1) as f32;
-    for (_, doc_frequency) in self.index_data.word_idf.iter_mut() {
-      *doc_frequency = (total_docs / *doc_frequency).ln() + 1.0;
-    }
     for (_, doc_frequency) in self.index_data.ngram_idf.iter_mut() {
       *doc_frequency = (total_docs / *doc_frequency).ln() + 1.0;
     }
 
     // Generate document vectors
-    let (word_vector, ngram_vector) = self.text_to_vectors(text);
+    let ngram_vector = self.text_to_vector(text);
     self.index_data.document_vectors.push(IndexedDocument {
       path: path.to_string(),
-      word_vector,
       ngram_vector,
     });
   }
@@ -212,20 +179,15 @@ impl VectorizationSystem {
     self.similarity_threshold
   }
 
-  fn tokenize(&self, text: &str) -> Vec<String> {
-    text.to_lowercase()
-        .split_whitespace()
-        .map(|s| s.to_string())
-        .collect()
-  }
-
   fn generate_ngrams(&self, text: &str) -> Vec<String> {
     let text = text.to_lowercase();
     let chars: Vec<char> = text.chars().collect();
     let mut ngrams = Vec::new();
 
-    for window in chars.windows(NGRAM_SIZE) {
-      ngrams.push(window.iter().collect::<String>());
+    for &size in &NGRAM_SIZES {
+      for window in chars.windows(size) {
+        ngrams.push(window.iter().collect::<String>());
+      }
     }
 
     ngrams
